4f29e60a03b93ef77e04a9d0178402a16d06671c
foodf <fo.fontus@duocuc.cl>
2026-05-15 02:06:13 -0400

    Termino con el MSCV product y su CRUD funcionando y agrego el MSCV del category con su CRUD funcionando, todo bien

---
+++ category-mscv/src/main/java/com/gamehubstore/category_mscv/controllers/ControllerCategory.java
@@ -0,0 +1,4 @@
+package com.gamehubstore.category_mscv.controllers;
+
+public class ControllerCategory {
+}
---
+++ category-mscv/src/main/java/com/gamehubstore/category_mscv/repositories/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.gamehubstore.category_mscv.repositories;
+
+public class CategoryRepository {
+}
